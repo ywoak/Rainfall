b3fe5324f4fef9a9721c0f5c48eeeab8d46b448a
Antoine-dev <aleduc@student.42.fr>
2023-03-04 01:01:47 +0100

    Bonus1 done, was easy after override7, did a pwntool script again, i think its clean this way

--- bonus1/source.c
+++ bonus1/source.c
@@ -1,13 +1,14 @@
 int main(inc ac, char **av)
 {
   // sub esp, 0x40
-  char buff[40]; // esp+
+  char buff[40]; // esp+0x14
   int atoi_ret; // esp+0x3c
 
   atoi_ret = atoi(av[1]);
   if (atoi_ret < 10) {
     memcpy(buff, av[2], atoi_ret * 4);
-    if (atoi_ret == "FLOW") { // 0x574f4c46
+    // Win block
+    if (atoi_ret == 0x574f4c46) { // FLOW in ascii
       execl("/bin/sh", sh, 0);
     }
     return 0;
